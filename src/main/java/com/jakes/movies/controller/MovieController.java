6ed0f4f255b38a9c97ce8faf7b8228a8591bd44b
JakeDasheck <daszek07@gmail.com>
2020-08-16 13:41:18 +0200

    getting movie from database

--- src/main/java/com/jakes/movies/controller/MovieController.java
+++ src/main/java/com/jakes/movies/controller/MovieController.java
@@ -1,13 +1,12 @@
-package com.jakes.movies.controllers;
+package com.jakes.movies.controller;
 
 import com.jakes.movies.model.Movie;
-import com.jakes.movies.services.MovieService;
+import com.jakes.movies.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
@@ -22,13 +21,22 @@ public class MovieController {
     @PostMapping
     @ResponseBody
     public Movie createMovie(@RequestBody Movie movie) {
-
         Movie createdMovie = service.createMovie(movie);
         System.out.println("New movie created: " + createdMovie);
         return createdMovie;
-
     }
 
+    @ResponseBody
+    @GetMapping(value = "{id}")
+    public Movie getMovie(@PathVariable Integer id) {
+        return service.getMovie(id);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/name/{name}")
+    public List<Movie> getMovieByName(@PathVariable String name) {
+        return service.getMovieByName(name);
+    }
 }
 
 
--- src/main/java/com/jakes/movies/service/MovieService.java
+++ src/main/java/com/jakes/movies/service/MovieService.java
@@ -1,9 +1,11 @@
-package com.jakes.movies.services;
+package com.jakes.movies.service;
 
 import com.jakes.movies.model.Movie;
 import com.jakes.movies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
@@ -15,5 +17,13 @@ public class MovieService {
 
     public Movie createMovie(Movie movie) {
         return repository.save(movie);
+    }
+
+    public Movie getMovie(Integer id) {
+        return repository.getOne(id);
+    }
+
+    public List<Movie> getMovieByName(String name) {
+        return repository.findMoviesByName(name);
     }
 }
